a3cf503b0985ef7410ee641b2c987315d04bc396
Aryan Katiyar <131390724+Katty020@users.noreply.github.com>
2024-02-25 22:21:49 +0530

    Update Button.js

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Button.scss";
-
+//exclusivee button
 export default function Button({text, className, href, newTab}) {
   return (
     <div className={className}>
